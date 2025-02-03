13ee2a06ed875fab568619204b26d013571495ff
Hakan ERSOY <hakan.ersoy@hotmail.com>
2025-02-03 13:20:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ async function searchData() {
     const searchInput = document.getElementById("searchInput").value;
     const resultDiv = document.getElementById("result");
     const data = await loadData();
-    const foundData = data.find(item => item.Code === searchInput);
+    const foundData = data.find(item => item.NACE === searchInput);
 
     if (foundData) {
-        resultDiv.innerHTML = `<p><strong>Açıklama:</strong> ${foundData.Description}</p><p><strong>Tehlike Sınıfı:</strong> ${foundData.RiskClass}</p>`;
+        resultDiv.innerHTML = `<p><strong>Açıklama:</strong> ${foundData.Tanım}</p><p><strong>Tehlike Sınıfı:</strong> ${foundData."Tehlike Sınıfı"}</p>`;
     } else {
         resultDiv.innerHTML = "<p>Veri bulunamadı.</p>";
     }
