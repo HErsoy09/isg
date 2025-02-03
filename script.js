3704aab182e5bc8b1966f534d2df7a6fe21e59c9
Hakan ERSOY <hakan.ersoy@hotmail.com>
2025-02-03 13:26:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ async function searchData() {
     const foundData = data.find(item => item.NACE === searchInput);
 
     if (foundData) {
-        resultDiv.innerHTML = `<p><strong>Açıklama:</strong> ${foundData.Tanım}</p><p><strong>Tehlike Sınıfı:</strong> ${foundData."Tehlike Sınıfı"}</p>`;
+        resultDiv.innerHTML = `<p><strong>Açıklama:</strong> ${foundData.Tanım}</p><p><strong>Tehlike Sınıfı:</strong> ${foundData["Tehlike Sınıfı"]}</p>`;
     } else {
         resultDiv.innerHTML = "<p>Veri bulunamadı.</p>";
     }
